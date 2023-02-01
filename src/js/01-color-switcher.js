e58bbf6fb68dd14b8a0f445fa9371e0142bb7887
ZhenianaKozhan <94985681+ZhenianaKozhan@users.noreply.github.com>
2023-02-01 10:34:35 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]')
+};
+
+refs.startBtn.addEventListener('click', onStartBtnClick)
+refs.stopBtn.addEventListener('click', onStopBtnClick)
+let timerId = null;
+
+refs.stopBtn.setAttribute('disabled', 0);
+
+function onStartBtnClick() {
+    timerId = setInterval(onChangeBodyBgrClr, 1000);
+    refs.startBtn.setAttribute('disabled', 0);
+    refs.stopBtn.removeAttribute('disabled');
+}
+
+function onStopBtnClick() {
+    clearInterval(timerId);
+    refs.startBtn.removeAttribute('disabled');
+    refs.stopBtn.setAttribute('disabled', 0);
+}
+
+function onChangeBodyBgrClr() {
+    document.body.style.backgroundColor = getRandomHexColor()
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
